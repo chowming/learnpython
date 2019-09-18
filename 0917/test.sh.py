dcd594e784f27c3a5d142b46917c85f2daa01566
chowming <givingdave@qq.com>
2019-09-18 20:14:12 +0800

    0918

--- 0917/test.sh.py
+++ 0917/test.sh.py
@@ -1,4 +1,20 @@
+#
+# test = input("input the name:")
+#
+# print("hello", test)
 
-test = input("input the name:")
 
-print("hello", test)
+print('hello world')
+print("hello", "world")
+print("1.123132131")
+print("1.123e9")
+
+print("I\' OK")
+
+print("I'am learn python")
+
+print('''hello world
+        hello python
+        day''')
+
+print(r"hello.\n")
